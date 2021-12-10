1a8160457c93e5f185a6637a83e4449e479a8f2d
opilane <opilane@HP-CZC6297BB7>
2021-12-10 12:58:22 +0200

    dd

--- Работа с словарем/Euroopa riigid.py
+++ Работа с словарем/Euroopa riigid.py
@@ -1,11 +1,21 @@
 from module1 import*
+from gtts import gTTS
+import os 
 
 countries_dict={"Albaania":"Tirana","Andorra":"Andorra la Vella","Armeenia":"Jerevan","Aserbaidžaan":"Bakuu","Austria":"Viin","Belgia":"Brüssel","Bulgaaria":"Sofia","Eesti":"Tallinn","Gruusia":"Thbilisi"}
 print(countries_dict )
+o=gTTS(text=countries_dict["Albaania"],lang="et",slow=True).save("heli.mp3")
+os.system("heli.mp3")
 while True:
     print("Tere,vaatame millised on riigid ja nende pealinnad")
-    print("1 - Uurige välja riigi pealinn või vastupidi")
+    print("1 - Uurige välja riigi pealinn või vastupidi,2 -uus pealinn ja riik,3- kustutamine,4-Vaatame teie teaded")
     s=input()
     if s=="1":
-        v=input("Kas otsime riiki pealinna -1 või pealinna 2- järgi?")
+        v=input("Kas otsime riiki pealinna (1) või pealinna järgi (2)? ")
         countries(countries_dict,v)
+    elif s=="2":
+        otsing(countries_dict)
+    elif s=="3":
+        kustutamine(countries_dict)
+    elif s=="4":
+        uuring(countries_dict)
--- Работа с словарем/module1.py
+++ Работа с словарем/module1.py
@@ -1,19 +1,46 @@
+from random import *
+for line in file:
+    k,v=line.strip().split("-")
+    TextFile1[k.strip()]=v.strip()
 def countries(d:dict, v:int):
     keys_list=(list(d.keys()))
     values_list=(list(d.values()))
     if v=="1":
-        p=[]
-        capital=(input("Pealinn: ")).capitalize()
-        capital.title()
+        s=[]
+        a=(input("Kirjuta Pealinn: ")).capitalize()
+        a.title()
         for i in values_list:
-            if i==capital:
+            if i==a:
                 for i in range(len(values_list)):
-                    if values_list[i]==capital:
-                        p.append(int(i))
+                    if values_list[i]==a:
+                        s.append(int(i))
                         print("Riik -", keys_list[i],":", "Pealinn -", values_list[i])
     else:
-        country=(input("Vali riiki: ")).capitalize()
-        a=d.get(country)
-        print("Riik -", country ,":", "Pealinn -", a)
+        c=(input("Kirjuta riik: ")).capitalize()
+        a=d.get(c)
+        print("Riik -", c ,":", "Pealinn -", a)
     return
+def otsing(countries_dict):
+    c=input("Kirjuta Pealinn=")
+    b=input("Kirjuta Riik=")
+    countries_dict.update({c: b})
+    print("Riik -", b ,":", "Pealinn -", c)
+def kustutamine(countries_dict):
+    b=input("Kirjutage Riiki mille tahate kustutada->")
+    countries_dict.pop(b)
+    print("Kustutatud Riik-",b,)
+def uuring(countries_dict):
+    v=int(input("Tere kas sa tahad kontrollima oma teadmisi? 1-Ja, 2-Ei"))
+    if v==1:
+        n=randint(0,9)
+        print(countries_dict[c[n])
+        print(countries_dict[c[randint(0,9)]])
+
+
+
+        
+
+    
+
+
 
